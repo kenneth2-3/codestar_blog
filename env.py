16ca10ad3d378c1ca89d29e181cc42dcfd615e92
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-08 15:51:30 +0100

    Enable serving of image files

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "<postgresql://neondb_owner:npg_94xJUKgdbtpY@ep-red-field-a23kjy9v.eu-central-1.aws.neon.tech/book_elbow_droop_902540>")
 os.environ.setdefault("SECRET_KEY", "<ada_is_tired_of_all>")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "" )
